aa07a7dc5bc7f349aae5f83dd9286ef835c9b425
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-04-20 13:56:30 -0400

    tabs.js is fully working

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -12,20 +12,20 @@ class TabLink {
     // Check to see if this.tabData is equal to 'all'
     if(this.tabData === 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
-      this.cards = document.querySelector('.card');
-      // console.log('yeet')
+      this.cards = document.querySelectorAll(`.card`);
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = document.querySelector(`.card[data-tab='${this.tabData}']`);
-      // console.log(this.cards)
+      this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
+      console.log(this.cards)
     }
+
     // <- Delete this comment block when you work on the if statement
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class.
-    this.cards = Array.from(this.cards).map(() => new TabCard(this.cards));
-
+    this.cards = Array.from(this.cards).map((card) => new TabCard(card));
     // Add a click event that invokes this.selectTab
     this.tabElement.addEventListener('click', () => {
+      console.log(`${this.tabData} was clicked`)
       this.selectTab();
     });
   }
